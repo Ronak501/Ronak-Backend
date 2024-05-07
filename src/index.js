ca05739da85fd5ba97f722224380336230513d1b
Ronak501 <ronaktalaviya51@gmail.com>
2024-05-07 18:44:17 +0530

    solve app error

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ dotenv.config({
 })
 
 connectDB()
-.then(
+.then(() => {
      app.on("error", (error) => {
-          console.log("ERROR: ",error);
+          console.log("Error: ",error);
           throw error
      }),
      app.listen(process.env.PORT || 8000, () => {
-          console.log(`\nApp is listening on port ${process.env.PORT}`);
+         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
      })
-)
+ }) 
 .catch((error) => {
      console.log("MONGO db connection failed !!!",error);
 })
