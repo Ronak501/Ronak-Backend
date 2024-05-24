2ff6076b7ecb037a24fd4426d176b9836a033e2f
Ronak501 <ronaktalaviya51@gmail.com>
2024-05-24 21:55:41 +0530

    fix bugs in middlerware and routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,9 @@
 import { Router} from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { 
+     loginUser,
+     logoutUser,
+     registerUser 
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
