24756e6cd780b02057a626ecb627c87855cccb4e
Ronak501 <ronaktalaviya51@gmail.com>
2024-06-10 23:07:47 +0530

    SOLVE some error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,149 +5,180 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 
 
-const generateAccessAndRefreshToken = async (userId) => {
-     try{
-          const user = await User.findById(userId)
-          const accessToken = user.generateAccessToken()
-          const refreshToken = user.generateRefreshToken()
-
-          user.refreshToken = refreshToken
-          await user.save({ValidateBeforeSave: false})
-
-          return{accessToken, refreshToken}
-
+const generateAccessAndRefereshTokens = async(userId) =>{
+     try {
+         const user = await User.findById(userId)
+         const accessToken = user.generateAccessToken()
+         const refreshToken = user.generateRefreshToken()
+ 
+         user.refreshToken = refreshToken
+         await user.save({ validateBeforeSave: false })
+ 
+         return {accessToken, refreshToken}
+ 
+ 
      } catch (error) {
-          throw new ApiError(500, "Something went wrong while generating access token")
+         throw new ApiError(500, "Something went wrong while generating referesh and access token")
      }
-}
-
-const registerUser = asyncHandler( async (req, res) => {
-     const {username, email, fullName, password} = req.body
-     //console.log(req.body)
-
-     if(
-          [username,fullName,email,password].some((field) => field?.trim() ==="")
-     ){
-          throw new ApiError(400, "All fields is required")
-     } 
-
+ }
+ 
+ const registerUser = asyncHandler( async (req, res) => {
+     // get user details from frontend
+     // validation - not empty
+     // check if user already exists: username, email
+     // check for images, check for avatar
+     // upload them to cloudinary, avatar
+     // create user object - create entry in db
+     // remove password and refresh token field from response
+     // check for user creation
+     // return res
+ 
+ 
+     const {fullName, email, username, password } = req.body
+     //console.log("email: ", email);
+ 
+     if (
+         [fullName, email, username, password].some((field) => field?.trim() === "")
+     ) {
+         throw new ApiError(400, "All fields are required")
+     }
+ 
      const existedUser = await User.findOne({
-          $or: [{username}, {email}]
+         $or: [{ username }, { email }]
      })
-
-     if(existedUser){
-          throw new ApiError(409, "User with email or username already exists")
+ 
+     if (existedUser) {
+         throw new ApiError(409, "User with email or username already exists")
      }
-
      //console.log(req.files);
+ 
      const avatarLocalPath = req.files?.avatar[0]?.path;
-     //const coverImageLocalPath =  req.files?.coverImage[0]?.path;
-
+     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+ 
      let coverImageLocalPath;
-     if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
-          coverImageLocalPath = req.files?.coverImage[0]?.path;
+     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+         coverImageLocalPath = req.files.coverImage[0].path
      }
-
-     if(!avatarLocalPath){
-          throw new ApiError(400,"Avatar File is required")
+     
+ 
+     if (!avatarLocalPath) {
+         throw new ApiError(400, "Avatar file is required")
      }
-
+ 
      const avatar = await uploadOnCloudinary(avatarLocalPath)
      const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-
-     if(!avatar){
-          throw new ApiError(400,"Avatar File is required")
+ 
+     if (!avatar) {
+         throw new ApiError(400, "Avatar file is required")
      }
-
+    
+ 
      const user = await User.create({
-          fullName,
-          avatar: avatar.url,
-          coverImage: coverImage?.url ||"",
-          email,
-          password,
-          username: username.toLowerCase()
+         fullName,
+         avatar: avatar.url,
+         coverImage: coverImage?.url || "",
+         email, 
+         password,
+         username: username.toLowerCase()
      })
-
+ 
      const createdUser = await User.findById(user._id).select(
-          "-password -refreshToken"
+         "-password -refreshToken"
      )
-
-     if(!createdUser){
-          throw new ApiError(500,"Something went wrong while registering the user.");
+ 
+     if (!createdUser) {
+         throw new ApiError(500, "Something went wrong while registering the user")
      }
-
+ 
      return res.status(201).json(
-          new ApiResponse(200, createdUser, "Created User Successfully")
+         new ApiResponse(200, createdUser, "User registered Successfully")
      )
-})
-
-const loginUser = asyncHandler( async (req, res) => {
-     const {email, username, password} =req.body
-     if(!username && !email){
-          throw new ApiError(400,"username or password is required")
+ 
+ } )
+ 
+ const loginUser = asyncHandler(async (req, res) =>{
+     // req body -> data
+     // username or email
+     //find the user
+     //password check
+     //access and referesh token
+     //send cookie
+ 
+     const {email, username, password} = req.body
+     console.log(email);
+ 
+     if (!username && !email) {
+         throw new ApiError(400, "username or email is required")
      }
-
-     const user = await User.findOne({
-          $or:[{username},{email}]
-     })
-  
-     if(!user){
-          throw new ApiError(404,"User does not exist")
-     }
-  
-     const isPasswordVaild =  await user.isPasswordCorrect(password)
-
-     if(!isPasswordVaild){
-          throw new ApiError(401,"invalid user credentials")
-     }
-
-     const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
-
-     const loggedInUser = await User.findbyId(user._id).select("-password -refreshToken")
      
-     const options = {
-          httpOnly: true,
-          secure: true
+     // Here is an alternative of above code based on logic discussed in video:
+     // if (!(username || email)) {
+     //     throw new ApiError(400, "username or email is required")
+         
+     // }
+ 
+     const user = await User.findOne({
+         $or: [{username}, {email}]
+     })
+ 
+     if (!user) {
+          throw new ApiError(404, "User does not exist")
      }
-
+ 
+    const isPasswordValid = await user.isPasswordCorrect(password)
+ 
+     if (!isPasswordValid) {
+          throw new ApiError(401, "Invalid user credentials")
+     }
+ 
+     const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+ 
+     const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+ 
+     const options = {
+         httpOnly: true,
+         secure: true
+     }
+ 
      return res
      .status(200)
      .cookie("accessToken", accessToken, options)
      .cookie("refreshToken", refreshToken, options)
      .json(
-          new ApiResponse(
-               200,
-               {
-                    user:loggedInUser, accessToken, refreshToken
-               },
-               "User logged in successfully"
-          )
+         new ApiResponse(
+             200, 
+             {
+                 user: loggedInUser, accessToken, refreshToken
+             },
+             "User logged In Successfully"
+         )
      )
-})
-
-const logoutUser = asyncHandler(async(req, res) => {
-     await User.findByAndUpdate(
-          req.user._id,
+ 
+ })
+ 
+ const logoutUser = asyncHandler(async(req, res) => {
+     await User.findByIdAndUpdate(
+     req.user._id,
           {
-               $set:{
-                    refreshToken: undefined
-               }
+             $unset: {
+               refreshToken: 1 // this removes the field from document
+             }
           },
           {
-               new: time
+             new: true
           }
-     )     
-
+     )
+ 
      const options = {
-          httpOnly: true,
-          secure: true
+         httpOnly: true,
+         secure: true
      }
-
+ 
      return res
      .status(200)
-     .clearcookie("accessToken", options)
-     .clearcookie("refreshToken", options)
-     .json(new ApiResponse(200,{},"User logged Out"))
-})
+     .clearCookie("accessToken", options)
+     .clearCookie("refreshToken", options)
+     .json(new ApiResponse(200, {}, "User logged Out"))
+ })
 
 export {registerUser,loginUser,logoutUser}
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,30 +1,29 @@
-import {v2 as cloudinary} from 'cloudinary'
-import { log } from 'console';
-import fs from 'fs'
-       
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEYS, 
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
-     try{
-          if(!localFilePath) return null
-          //upload the file on cloudinary
-          const response = await cloudinary.uploader.upload(localFilePath,{
-               resource_type: "auto"
-          })
-          //file has been uploaded successfull
-          //console.log("file is uploaded on cloudinary",response.url);
-          //console.log(response);
-          fs.unlinkSync(localFilePath)
-          return response;
-     }    catch(error){
-          fs.unlinkSync(localFilePath)
-          //remove file from server
-          return null;
-     }
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
 }
 
-export {uploadOnCloudinary};
+export {uploadOnCloudinary}
